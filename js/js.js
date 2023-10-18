cfb552d2ad6a5e2a1204ce89d7ef7172728de16f
Jose Luis <jltortolacer@gmail.com>
2023-10-18 19:14:20 +0200

    Index practicamente terminado version Movil

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function mostrarInformacion() {
+    var elemento = document.getElementById("tarjetaInformacion1");
+    elemento.classList.toggle("active");
+}
+
+function mostrarInformacionDos(){
+    var elemento = document.getElementById("tarjetaInformacion2");
+    elemento.classList.toggle("active");
+}
